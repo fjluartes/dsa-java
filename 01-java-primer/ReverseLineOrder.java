bbe6883f66de1c13e4bef1d6ec97ad7c83dfd537
Fred Luartes <fjluartes@gmail.com>
2023-08-29 21:02:22 +0800

    Adds answer to ReverseLineOrder exercise

--- 01-java-primer/ReverseLineOrder.java
+++ 01-java-primer/ReverseLineOrder.java
@@ -1,6 +1,21 @@
 // P1.26 Reverse Line Order
+import java.util.Scanner;
+
 public class ReverseLineOrder {
     public static void main(String[] args) {
-        System.out.println("Reverse Line Order");
+        String str = "";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a line of string: "); 
+        while(!input.hasNextInt()) {
+            String newStr = "";
+            char c;
+            String temp = input.nextLine();
+            for (int i = 0; i < temp.length(); i++) {
+                c = temp.charAt(i);
+                newStr = c + newStr;
+            }
+            str += newStr + "\n";
+        }
+        System.out.println(str);
     }
 }
