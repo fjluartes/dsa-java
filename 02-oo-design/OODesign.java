23a46671aa96cd5fe5a2b047c74374614ab0948a
Fred Luartes <fjluartes@gmail.com>
2023-08-30 12:25:45 +0800

    Edits OODesign:

--- 02-oo-design/OODesign.java
+++ 02-oo-design/OODesign.java
@@ -1,6 +1,6 @@
 // Object Oriented Design
 public class OODesign {
     public static void main(String[] args) {
-        System.out.println("Hello Universe!");
+        System.out.println("Object Oriented Design!");
     }
 }
