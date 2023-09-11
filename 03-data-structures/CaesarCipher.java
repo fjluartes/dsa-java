bb5a71645160032b5fb689af04a34c39662b798e
Fred Luartes <fjluartes@gmail.com>
2023-09-11 14:10:53 +0800

    Adds CaesarCipher partial code

--- 03-data-structures/CaesarCipher.java
+++ 03-data-structures/CaesarCipher.java
@@ -1,5 +1,31 @@
-// Caesar Cipher in Java
+/* Class for doing encryption and decryption using Caesar Cipher */
 public class CaesarCipher {
+    protected char[] encoder = new char[26];
+    protected char[] decoder = new char[26];
+
+    // constructor for encryption/decryption arrays
+    public CaesarCipher(int rotation) {
+        for (int k = 0; k < 26; k++) {
+            encoder[k] = (char) ('A' + (k + rotation) % 26);
+            decoder[k] = (char) ('A' + (k - rotation + 26) % 26);
+        }
+    }
+
+    // returns String representing encrypted message
+    public String encrypt(String message) {
+        return transform(message, encoder);
+    }
+
+    // returns decrypted message given encrypted secret
+    public String decrypt(String secret) {
+        return transform(secret, decoder);
+    }
+
+    // returns transformation fo original String using given code
+    private String transform(String original, char[] code) {
+        // code to transform
+    }
+    
     public static void main(String[] args) {
         System.out.println("Caesar Cipher");
     }
